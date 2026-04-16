9cc64438029e6b528ebe455a0e8589a9f63cd2e1
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2026-04-16 02:35:04 +0100

    start study js with variables and operators

---
+++ variables-and-operators/my-script.js
@@ -0,0 +1,17 @@
+//LET -- we can re-assign
+
+let age = 11;
+console.log(age);
+
+age = 66;
+console.log
+
+console.log(age);
+
+// CONST -- can't re-assign
+const pi = 3.14;
+//pi = 12;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
